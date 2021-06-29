fd0e7a341f854ec9a1f173a5d071ffe8e7dd7eb7
FabricioCruzz <fabriciocruzz77@gmail.com>
2021-06-28 21:56:20 -0300

    Prova 2 add

---
+++ Prova2-tp3-Fabricio-Adriano-da-Cruz/src/br/edu/univas/controller/CustomerController.java
@@ -0,0 +1,82 @@
+package br.edu.univas.controller;
+
+import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.Scanner;
+
+import br.edu.univas.exception.PaymentMethodNotAllowedException;
+import br.edu.univas.model.Customer;
+import br.edu.univas.vo.AccountDebit;
+import br.edu.univas.vo.BankSlip;
+import br.edu.univas.vo.CreditCard;
+import br.edu.univas.vo.PaymentMethod;
+import br.edu.univas.vo.Pix;
+
+public class CustomerController {
+	
+	private Scanner sc;
+	private Random rd;
+	
+	public Customer createCustomer() {
+		
+		try {
+			sc = new Scanner(System.in);			
+			
+			rd = new Random();
+			
+			Customer customer = new Customer();
+			
+//			Coloquei o random para gerar um ID aleatório de long
+			System.out.println("Gerando ID...");
+			customer.setId(rd.nextLong());
+			
+			System.out.println("Digite um nome:");
+			customer.setName(sc.nextLine());
+			
+			System.out.println("Digite o CPF:");
+			customer.setCpf(sc.nextLine());
+			
+			System.out.println("Digite um método de pagamento:");
+			String s = sc.nextLine();
+			PaymentMethod pm = null;
+			
+			if(s.equals(CreditCard.class.getSimpleName())) {
+				CreditCard creditCard = new CreditCard();
+				pm = creditCard;
+			}
+			else if(s.equals(AccountDebit.class.getSimpleName())) {
+				AccountDebit accountDebit = new AccountDebit();
+				pm = accountDebit;
+			}
+			else if(s.equals(Pix.class.getSimpleName())) {
+				Pix pix = new Pix();
+				pm = pix;
+			}
+			else if(s.equals(BankSlip.class.getSimpleName())) {
+				BankSlip bankSlip = new BankSlip();
+				pm = bankSlip;
+			}
+			
+			customer.setPaymentMethod(pm);
+			
+			System.out.println("Customer criado com sucesso!");
+			return customer;
+			
+		} catch (PaymentMethodNotAllowedException e) {
+			e.setMessage("Erro na opção de pagamento!");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			sc = new Scanner(System.in);
+		}
+		finally {
+			sc.close();
+		}
+		
+	return null;
+	
+	}
+
+}
